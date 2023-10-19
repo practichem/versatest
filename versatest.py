8826c3f555165c08a301b05f0c7bca528a60b08b
Nick DeMarco <nick@demarcohome.com>
2023-10-19 04:45:25 -0400

    More pythonic

--- versatest.py
+++ versatest.py
@@ -1,94 +1,85 @@
 def percent_to_8bit(percent):
-    '''Converts a percentage (0.0 to 100.0%) to an 8 bit bytearray.'''
-    if (percent > 0.0) & (percent <= 100.0): 
-        eightBitPercent = round(percent * 2.55)
-        return eightBitPercent        
-    else:
-        return 0
+    '''Converts a percentage (0.0 to 100.0%) to an 8-bit bytearray.'''
+    if 0.0 <= percent <= 100.0:
+        eight_bit_percent = round(percent * 2.55)
+        return eight_bit_percent
+    return 0
 
-def makePacket(command, parameter):
-    packet = bytearray()
-    packet.append(command)
-    packet.append(parameter)
-    return packet
+def make_packet(command, parameter):
+    return bytearray([command, parameter])
 
-def set_speed(dir, speed):  # Sets the speed. Dir is up or down. 0xFF = 100%
+def set_speed(direction, speed):
+    # Sets the speed. Direction is 'up' or 'down'. 0xFF = 100%
     command = 0
     parameter = percent_to_8bit(speed)
-    if dir == 'up':
+    
+    if direction == 'up':
         command = 0x1
-    elif dir == 'down':
+    elif direction == 'down':
         command = 0x2
-    return makePacket(command, parameter)
+    
+    return make_packet(command, parameter)
 
 def set_mode(mode):
     command = 0
-    if mode == 'manual':
-        command = 0x80
-    elif mode == 'single_cycle':
-        command = 0x81
-    elif mode == 'cont_cycle':
-        command = 0x82
-    return command.to_bytes(1,'big')
-
-def set_go(mode):          # Simulates the up/down switch: neutral, up, down
-    command = 0
-    if mode == 'neutral':
-        command = 0x90
-    elif mode == 'up':
-        command = 0x91
-    elif mode == 'down':
-        command = 0x92
-    return command.to_bytes(1,'big')
-
-def set_limit(mode):       # Simulates the travel limit switch set: open, up_limit, down_limit
-    command = 0
-    if mode == 'open':
-        command = 0xa0
-    elif mode == 'up_limit':
-        command = 0xa1
-    elif mode == 'down_limit':
-        command = 0xa2
-    return command.to_bytes(1,'big')
-
-def set_control(mode):        # Sets the control mode: remote, local. In 'local', all commands are ignored except 'remote'
-    command = 0
-    if mode == 'remote':
-        command = 0xf2
-    elif mode == 'local':
-        command = 0xf3
-    return command.to_bytes(1,'big')
     
+    mode_mapping = {
+        'manual': 0x80,
+        'single_cycle': 0x81,
+        'cont_cycle': 0x82
+    }
+    
+    return mode_mapping.get(mode, 0).to_bytes(1, 'big')
+
+def set_go(mode):
+    command = 0
+    
+    mode_mapping = {
+        'neutral': 0x90,
+        'up': 0x91,
+        'down': 0x92
+    }
+    
+    return mode_mapping.get(mode, 0).to_bytes(1, 'big')
+
+def set_limit(mode):
+    command = 0
+    
+    mode_mapping = {
+        'open': 0xa0,
+        'up_limit': 0xa1,
+        'down_limit': 0xa2
+    }
+    
+    return mode_mapping.get(mode, 0).to_bytes(1, 'big')
+
+def set_control(mode):
+    command = 0
+    
+    mode_mapping = {
+        'remote': 0xf2,
+        'local': 0xf3
+    }
+    
+    return mode_mapping.get(mode, 0).to_bytes(1, 'big')
+
 def get_switches_status():
     # Returns simulated switch status.
-        command = 0xe0
-        # Send command to port.
-        # Interpret response from port.
-        # Create object containing interpreted responses.
-        return command
+    return 0xe0.to_bytes(1, 'big')
 
-def get_up_speed_setpoint():            # Returns set speed for up.
-        command = 0xe1
-        return command
+def get_up_speed_setpoint():
+    # Returns set speed for up.
+    return 0xe1.to_bytes(1, 'big')
 
-def get_down_speed_setpoint():          # Returns set speed for down.
-        command = 0xe2
-        return command
+def get_down_speed_setpoint():
+    # Returns set speed for down.
+    return 0xe2.to_bytes(1, 'big')
 
 def get_run_state():
-        command = 0xe8
-        return command        
+    return 0xe8.to_bytes(1, 'big')
 
 def get_run_speed():
-        command = 0xe9
-        return command
+    return 0xe9.to_bytes(1, 'big')
 
 def get_version():
-        command = 0xEF
-        return command
-
-
-# https://stackoverflow.com/questions/17553543/pyserial-non-blocking-read-loop/38758773
-
-
-
+    return 0xEF.to_bytes(1, 'big')
